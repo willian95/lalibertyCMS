396998b52801c32c731de004dcf2dc30aa4b743a
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-08 10:25:16 -0400

    cambios

--- resources/views/works/create.blade.php
+++ resources/views/works/create.blade.php
@@ -22,11 +22,9 @@
                     
                         <div class="row">
                                     
-                            
-                           
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="title">Titulo</label>
+                                        <label for="title">Título</label>
                                         <input type="text" class="form-control" v-model="title" name="title">
                                         <small v-if="errors.hasOwnProperty('title')">@{{ errors['title'][0] }}</small>
                                     </div>
@@ -51,7 +49,7 @@
 
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="image">Imagen</label>
+                                        <label for="image">Imágen</label>
                                         <input type="file" class="form-control" ref="file" @change="onImageChange" accept="image/*" style="overflow: hidden;" name="image">
 
                                         <img id="blah" :src="imagePreview" class="full-image" style="margin-top: 10px; width: 40%">
@@ -62,7 +60,7 @@
                                         
                                         <p v-if="pictureStatus == 'subiendo' && imageProgress < 100">Subiendo</p>
                                         <p v-if="pictureStatus == 'subiendo' && imageProgress == 100">Espere un momento</p>
-                                        <p v-if="pictureStatus == 'listo' && imageProgress == 100">Imagen lista</p>
+                                        <p v-if="pictureStatus == 'listo' && imageProgress == 100">Imágen lista</p>
 
                                         <small v-if="errors.hasOwnProperty('image')">@{{ errors['image'][0] }}</small>
                                     </div>
@@ -91,7 +89,7 @@
                    
                     <div class="row">
                         <div class="col-12">
-                            <h3 class="text-center">Imagenes secundarias <button @click="create()" class="btn btn-success" data-toggle="modal" data-target="#presentationModal">+</button></h3>
+                            <h3 class="text-center">Imágenes secundarias <button @click="create()" class="btn btn-success" data-toggle="modal" data-target="#presentationModal">+</button></h3>
                         </div>
 
                     </div>
@@ -126,7 +124,7 @@
                                            
                                             <p v-if="workImage.status == 'subiendo' && workImage.progress < 100">Subiendo</p>
                                             <p v-if="workImage.status == 'subiendo' && workImage.progress == 100">Espere un momento</p>
-                                            <p v-if="workImage.status == 'listo' && workImage.progress == 100">Imagen lista</p>
+                                            <p v-if="workImage.status == 'listo' && workImage.progress == 100">Imágen lista</p>
                                         </td>
                                         <td>
                                             <button class="btn btn-danger" @click="deleteWorkImage(index)"><i class="far fa-trash-alt"></i></button>
@@ -171,7 +169,7 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <label for="type">Imagen o video</label>
+                                    <label for="type">Imágen o video</label>
                                     <input type="file" class="form-control" ref="file" @change="onSecondaryImageChange" accept="image/*|video/*" style="overflow: hidden;">
                                     <img id="blah" :src="secondaryPreviewPicture" class="full-image" style="margin-top: 10px; width: 40%">
 
@@ -433,7 +431,7 @@
                     }else{
                         swal({
                             title: "Oppss!",
-                            text: "Debes añadir una imagen",
+                            text: "Debes añadir una imágen",
                             icon: "error"
                         });
                     }
