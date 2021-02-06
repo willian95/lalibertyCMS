47bd83f7220586b1ad60c53c14bd703bf672d0ca
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-02-05 22:41:12 -0400

    backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('daily:backup')->dailyAt("02:50");
+        $schedule->command('daily:backup')->dailyAt("02:43");
     }
 
     /**
