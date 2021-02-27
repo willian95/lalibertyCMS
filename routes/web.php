7ad036be7861f1b463314887b29839b40354bed8
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-02-26 21:18:58 -0400

    cambios

--- routes/web.php
+++ routes/web.php
@@ -144,20 +144,20 @@ Route::get("clear-cloudinary", function(){
 
 Route::get("q_auto", function(){
 
-    $workImages = App\Blog::all();
+    $workImages = App\Category::all();
 
     foreach($workImages as $workImage){
         
         if(strpos($workImage->image, "/image/upload") > 0){
 
-            $image = App\Blog::find($workImage->id);
-            $image->image = str_replace("/image/upload", "image/upload/qr_auto", $image->image);
+            $image = App\Category::find($workImage->id);
+            $image->image = str_replace("/image/upload", "/image/upload/q_auto", $image->image);
             $image->update();
 
         }else if(strpos($workImage->image, "/video/upload") > 0){
 
-            $image = App\Blog::find($workImage->id);
-            $image->image = str_replace("/video/upload", "video/upload/qr_auto", $image->image);
+            $image = App\Category::find($workImage->id);
+            $image->image = str_replace("/video/upload", "/video/upload/q_auto", $image->image);
             $image->update();
 
         }
